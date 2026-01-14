ed7a57b2f8291c4841b6a6e75a5f0758a77d0281
shinkwang <shinkwang@yonsei.ac.kr>
2026-01-14 21:11:54 +0900

    Chapter 2: Function

--- src/main/java/com/lannstark/lec08/Lec08Main.java
+++ src/main/java/com/lannstark/lec08/Lec08Main.java
@@ -6,6 +6,24 @@ public class Lec08Main {
 
   }
 
+  public void repeat(String str, int num, boolean useNewLine) {
+    for (int i = 0; i< num; i++) {
+      if (useNewLine) {
+        System.out.println(str);
+      }  else {
+        System.out.print(str);
+      }
+    }
+  }
+
+  public void repeat(String str, int num) {
+    repeat(str, num, true);
+  }
+
+  public void repeat(String str) {
+    repeat(str, 3, true);
+  }
+
   public static void printAll(String... strings) {
     for (String str : strings) {
       System.out.println(str);
---
+++ src/main/kotlin/com/shin/lec08/lec08Main.kt
@@ -0,0 +1,17 @@
+package com.shin.lec08
+
+fun main() {
+    repeat("Hello World", 3, true)
+    repeat("Hello World")
+    repeat("Hello World", useNewLine = false)
+}
+
+fun repeat(str: String, num: Int = 3, useNewLine: Boolean = true) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
